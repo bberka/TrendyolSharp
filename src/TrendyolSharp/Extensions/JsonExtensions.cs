2e0ffc337e3f610c963d8ca5470bfad6609d88c9
bberka <berkaybyr@icloud.com>
2024-04-22 16:37:40 +0300

    fix: json read settings

--- src/TrendyolSharp/Extensions/JsonExtensions.cs
+++ src/TrendyolSharp/Extensions/JsonExtensions.cs
@@ -4,20 +4,25 @@ namespace TrendyolSharp.Extensions
 {
   public static class JsonExtensions
   {
+    private static JsonSerializerSettings _settings = new JsonSerializerSettings
+    {
+      ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver
+      {
+        NamingStrategy = new Newtonsoft.Json.Serialization.CamelCaseNamingStrategy()
+      },
+      TypeNameHandling = TypeNameHandling.Auto,
+      NullValueHandling = NullValueHandling.Ignore,
+      Converters = { new Newtonsoft.Json.Converters.StringEnumConverter() },
+      Formatting = Formatting.Indented
+    };
     public static string ToJsonString(this object obj) {
-      return JsonConvert.SerializeObject(obj, Formatting.Indented);
-    }
-
-    public static string ToJsonString(this object obj, Formatting formatting) {
-      return JsonConvert.SerializeObject(obj, formatting);
+      return JsonConvert.SerializeObject(obj, _settings);
     }
 
     public static T ToObject<T>(this string json) {
-      return JsonConvert.DeserializeObject<T>(json);
+      return JsonConvert.DeserializeObject<T>(json,_settings);
     }
 
-    public static T ToObject<T>(this string json, JsonSerializerSettings settings) {
-      return JsonConvert.DeserializeObject<T>(json, settings);
-    }
+
   }
 }
