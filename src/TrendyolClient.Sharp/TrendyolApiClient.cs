4c4d04754bfa40ced19a2286c0047fde1ab8dac8
Berkay <77205615+bberka@users.noreply.github.com>
2025-03-07 01:15:33 +0300

    feat: db api key options

--- src/TrendyolClient.Sharp/TrendyolApiClient.cs
+++ src/TrendyolClient.Sharp/TrendyolApiClient.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Serilog;
-using TrendyolClient.Sharp.Models;
 using TrendyolClient.Sharp.Services.Marketplace;
 
 // using TrendyolClient.Sharp.Services.Finance;
@@ -29,13 +30,29 @@ namespace TrendyolClient.Sharp
                              string integrationCompanyNameForHeader = "SelfIntegration",
                              bool useStageApi = false,
                              ILogger logger = null) {
+      if (sellerId < 1) throw new ArgumentNullException(nameof(sellerId));
       SellerId = sellerId;
       ApiKey = apiKey;
       ApiSecret = apiSecret;
       UseStageApi = useStageApi;
       _logger = logger;
-      var httpClient = new HttpClient();
-      if (SellerId < 1) throw new ArgumentNullException(nameof(sellerId));
+
+      var cookieContainer = new CookieContainer();
+      var handler = new HttpClientHandler {
+        UseCookies = true,
+        CookieContainer = cookieContainer,
+        AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+      };
+
+
+      var httpClient = new HttpClient(handler);
+
+      httpClient.DefaultRequestHeaders.ConnectionClose = false;
+      httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+      httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
+      httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("tr-TR"));
+
+      httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36");
 
       httpClient.DefaultRequestHeaders.Add("User-Agent", $"{sellerId} - {integrationCompanyNameForHeader}");
       httpClient.DefaultRequestHeaders.Add("Authorization", $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes($"{apiKey}:{apiSecret}"))}");
